d7680b2cbf9813ebe456fa45543d24031d4d13e1
syanmi <syanmims@gmail.com>
2024-08-03 07:40:58 +0900

    add

--- OneSearch.Extensibility/Core/Services/IServiceCollectionExtensions.cs
+++ OneSearch.Extensibility/Core/Services/IServiceCollectionExtensions.cs
@@ -5,14 +5,17 @@ namespace OneSearch.Extensibility.Core.Services
     public static class IServiceCollectionExtensions
     {
         public static void AddSingleton<T>(this IServiceCollection collection) where T : class
-            => collection.Add<T>(null, ServiceLifeTime.Singleton);
-
+            => collection.Add(typeof(T), typeof(T), ServiceLifeTime.Singleton);
+        public static void AddSingleton<T1, T2>(this IServiceCollection collection) where T1 : class where T2 : class
+            => collection.Add(typeof(T1), typeof(T2), ServiceLifeTime.Singleton);
         public static void AddSingleton<T>(this IServiceCollection collection, Func<IServiceProvider, T> factory) where T : class
             => collection.Add(factory, ServiceLifeTime.Singleton);
 
-        public static void AddTransient<T>(this IServiceCollection collection) where T : class
-            => collection.Add<T>(null, ServiceLifeTime.Transient);
 
+        public static void AddTransient<T>(this IServiceCollection collection) where T : class
+            => collection.Add(typeof(T), typeof(T), ServiceLifeTime.Transient);
+        public static void AddTransient<T1, T2>(this IServiceCollection collection) where T1 : class where T2 : class
+            => collection.Add(typeof(T1), typeof(T2), ServiceLifeTime.Transient);
         public static void AddTransient<T>(this IServiceCollection collection, Func<IServiceProvider, T> factory) where T : class
             => collection.Add(factory, ServiceLifeTime.Transient);
     }
