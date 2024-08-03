e462200e502179c9dfc8ff861c141e4455fe0dc3
syanmi <syanmims@gmail.com>
2024-08-03 22:23:37 +0900

    add

--- OneSearch.Extensibility/Core/Data/XDocumentDataSource.cs
+++ OneSearch.Extensibility/Core/Data/XDocumentDataSource.cs
@@ -8,27 +8,26 @@ namespace OneSearch.Extensibility.Core.Data
 {
     public class XDocumentDataSource : IDataSource
     {
-        private XmlDocument _file;
+        private XDocument _file;
         private string _path;
 
         private XDocumentDataSource(string path)
         {
-            _file = new XmlDocument();
+            _file = null;
             _path = path;
         }
 
         public T GetSection<T>() where T : new()
         {
-            string filePath = _path;
-            if (!File.Exists(filePath))
+            if (!File.Exists(_path))
             {
-                return new T(); // ファイルが存在しない場合、新しいインスタンスを返す
+                return new T();
             }
 
-            XDocument doc = XDocument.Load(filePath);
+            _file = _file ?? XDocument.Load(_path);
 
             string elementName = typeof(T).GetCustomAttributes(typeof(XmlRootAttribute), false) is XmlRootAttribute[] attributes && attributes.Length > 0 ? attributes[0].ElementName : typeof(T).Name;
-            XElement element = doc.Root.Element(elementName);
+            XElement element = _file.Root.Element(elementName);
 
             if (element != null)
             {
@@ -36,29 +35,24 @@ namespace OneSearch.Extensibility.Core.Data
             }
             else
             {
-                return new T(); // 要素が存在しない場合、新しいインスタンスを返す
+                return new T();
             }
         }
 
         public void SetSection<T>(T section) where T : new()
         {
-            string filePath = _path;
-            XDocument doc;
-
-            if (File.Exists(filePath))
+            if (_file == null)
             {
-                doc = XDocument.Load(filePath);
-            }
-            else
-            {
-                doc = new XDocument(new XElement("Root"));
+                _file = (File.Exists(_path)) ? 
+                    XDocument.Load(_path) : 
+                    new XDocument(new XElement("Root"));
             }
 
             string elementName = typeof(T).GetCustomAttributes(typeof(XmlRootAttribute), false) is XmlRootAttribute[] attributes && attributes.Length > 0 ? attributes[0].ElementName : typeof(T).Name;
             string xmlString = XmlHelper.XMLSerialize(section);
 
             XElement newElement = XElement.Parse(xmlString);
-            XElement existingElement = doc.Root.Element(elementName);
+            XElement existingElement = _file.Root.Element(elementName);
 
             if (existingElement != null)
             {
@@ -66,33 +60,26 @@ namespace OneSearch.Extensibility.Core.Data
             }
             else
             {
-                doc.Root.Add(newElement);
+                _file.Root.Add(newElement);
             }
-
-            doc.Save(filePath);
-            Console.WriteLine($"XML saved successfully for {typeof(T).Name}.");
         }
 
         public void Save()
         {
-            // _file.Save(_path);
+            _file?.Save(_path);
         }
 
         private void Load()
         {
             if (File.Exists(_path))
             {
-                _file.Load(_path);
+                _file = XDocument.Load(_path);
             }
         }
 
         public static XDocumentDataSource Load(string path)
         {
-            var source = new XDocumentDataSource(path);
-
-            source.Load();
-
-            return source;
+            return new XDocumentDataSource(path);
         }
     }
 }
