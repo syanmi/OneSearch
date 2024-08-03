ac400df8c4996f08ca25dc2f885fc51a6c9cf945
syanmi <syanmims@gmail.com>
2024-08-03 22:04:23 +0900

    add

--- OneSearch.Extensibility/Core/Data/XmlFileDataSource.cs
+++ OneSearch.Extensibility/Core/Data/XmlFileDataSource.cs
@@ -19,17 +19,15 @@ namespace OneSearch.Extensibility.Core.Data
         public T GetSection<T>() where T : new()
         {
             string filePath = _path;
-            XmlDocument doc = new XmlDocument();
 
-            if (!System.IO.File.Exists(filePath))
+            if (!File.Exists(filePath))
             {
-                return new T(); // ファイルが存在しない場合、新しいインスタンスを返す
+                return new T();
             }
 
-            doc.Load(filePath);
-
             string elementName = typeof(T).GetCustomAttributes(typeof(XmlRootAttribute), false) is XmlRootAttribute[] attributes && attributes.Length > 0 ? attributes[0].ElementName : typeof(T).Name;
-            XmlNode node = doc.DocumentElement.SelectSingleNode(elementName);
+            XmlNode node = _file.DocumentElement.SelectSingleNode(elementName);
+
 
             if (node != null)
             {
@@ -45,16 +43,11 @@ namespace OneSearch.Extensibility.Core.Data
         public void SetSection<T>(T section)
         {
             string filePath = _path;
-            XmlDocument doc = new XmlDocument();
 
-            if (System.IO.File.Exists(filePath))
+            if (_file.DocumentElement == null)
             {
-                doc.Load(filePath);
-            }
-            else
-            {
-                XmlElement root = doc.CreateElement("Root");
-                doc.AppendChild(root);
+                XmlElement root = _file.CreateElement("Root");
+                _file.AppendChild(root);
             }
 
             string elementName = typeof(T).GetCustomAttributes(typeof(XmlRootAttribute), false) is XmlRootAttribute[] attributes && attributes.Length > 0 ? attributes[0].ElementName : typeof(T).Name;
@@ -64,31 +57,42 @@ namespace OneSearch.Extensibility.Core.Data
             tempDoc.LoadXml(xmlString);
             XmlNode newNode = tempDoc.DocumentElement;
 
-            XmlNode existingNode = doc.DocumentElement.SelectSingleNode(elementName);
+            XmlNode existingNode = _file.DocumentElement.SelectSingleNode(elementName);
             if (existingNode != null)
             {
-                doc.DocumentElement.ReplaceChild(doc.ImportNode(newNode, true), existingNode);
+                _file.DocumentElement.ReplaceChild(_file.ImportNode(newNode, true), existingNode);
             }
             else
             {
-                doc.DocumentElement.AppendChild(doc.ImportNode(newNode, true));
+                _file.DocumentElement.AppendChild(_file.ImportNode(newNode, true));
             }
 
-            doc.Save(filePath);
             Console.WriteLine($"XML saved successfully for {typeof(T).Name}.");
         }
 
         public void Save()
         {
-           // _file.Save(_path);
+            _file.Save(_path);
+        }
+
+        private void Load()
+        {
+            if (File.Exists(_path))
+            {
+                _file.Load(_path);
+            }
         }
 
         public static XmlFileDataSource Load(string path)
         {
-            return new XmlFileDataSource(path);
+            var source = new XmlFileDataSource(path);
+
+            source.Load();
+
+            return source;
         }
 
-        public static string XMLSerialize<T>(T obj)
+        private static string XMLSerialize<T>(T obj)
         {
             using (StringWriter stringWriter = new StringWriter())
             {
